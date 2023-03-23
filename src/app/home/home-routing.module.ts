0c7ef7a91370d5926269127dd0ccc03b93f02cfc
vignesh <vignesh.b@solvedge.in>
2023-03-23 19:13:37 +0530

    gentleman

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './landing-page/about/about.component';
+import { FaqComponent } from './landing-page/faq/faq.component';
+import { FeaturesComponent } from './landing-page/features/features.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PricingComponent } from './landing-page/pricing/pricing.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'home', pathMatch:'full'},
-  {path : 'home', component: LandingPageComponent},
-  {path:'login', loadChildren : ()=> import('../auth/auth.module').then(authMod=> authMod.AuthModule)}
+  {path : 'home', component: LandingPageComponent,
+  children:[
+    {path : 'home', component: LandingPageComponent},
+    {path:'features',component:FeaturesComponent},
+    {path:'pricing',component:PricingComponent},
+    {path:'faqs',component:FaqComponent},
+    {path:'about',component:AboutComponent}]
+},
+{path:'login', loadChildren:()=> import('../auth/auth.module').then(authMod=> authMod.AuthModule)}
 ];
 
 @NgModule({
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { FeaturesComponent } from './landing-page/features/features.component';
+import { PricingComponent } from './landing-page/pricing/pricing.component';
+import { FaqComponent } from './landing-page/faq/faq.component';
+import { AboutComponent } from './landing-page/about/about.component';
 
 
 @NgModule({
   declarations: [
-    LandingPageComponent
+    LandingPageComponent,
+    FeaturesComponent,
+    PricingComponent,
+    FaqComponent,
+    AboutComponent
   ],
   imports: [
     CommonModule,
