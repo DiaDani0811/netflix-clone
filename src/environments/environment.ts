0bab4776cab9484dd971cbfa2da2b38b624efa18
vignesh <vignesh.b@solvedge.in>
2023-04-05 19:08:14 +0530

    chang

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  JsonserverSwags:"http://localhost:4200",
+  JsonserverSwags:"http://localhost:3000",
   production: false
 };
 
