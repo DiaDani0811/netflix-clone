15fec27399c113624b169afba4aa067eab3bd038
vigneshb03 <95528831+vigneshb03@users.noreply.github.com>
2024-02-26 23:41:00 +0530

    nyc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LogoLoaderComponent } from './logo-loader/logo-loader.component';
 const routes: Routes = [
 {path:'', redirectTo:'Loading', pathMatch:'full'},
 {path:'Loading',component:LogoLoaderComponent},
+
 {path:'home',loadChildren:() =>import('../app/home/home.module').then(module=>module.HomeModule)},
 {path:'auth',loadChildren:()=>import('../app/auth/auth.module').then(auth=>auth.AuthModule)},
 ]
