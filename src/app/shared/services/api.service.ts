40bd7611ddc345b299f1e2fc7da5cd271854b6b0
vigneshb03 <95528831+vigneshb03@users.noreply.github.com>
2024-08-09 08:45:41 +0530

    sdfrsfd

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
@@ -10,23 +10,23 @@ export class ApiService {
   constructor(private http:HttpClient) { }
 
   post(path: string, body: Object={}){
-   return this.http.post(`${environment.api_url}${path}`,body).pipe(res=>{
+   return this.http.post(`${environment.api_url}${path}`,body).pipe((res: any)=>{
     return res;
    })
   }
   get(path: string, params: HttpParams = new HttpParams()): Observable<any>{
     let headers = new HttpHeaders
-    return this.http.get(`${environment.api_url}${path}`,{params,headers:headers}).pipe(res=>{
+    return this.http.get(`${environment.api_url}${path}`,{params,headers:headers}).pipe((res: any)=>{
       return res;
     });
   }
   update(path: string, body: Object={}){
-    return this.http.put(`${environment.api_url}${path}`,body).pipe(res=>{
+    return this.http.put(`${environment.api_url}${path}`,body).pipe((res: any)=>{
       return res;
     })
  }
  delete(path: string,id:number){
-  return this.http.delete(`${environment.api_url}${path}`+id).pipe(res=>{
+  return this.http.delete(`${environment.api_url}${path}`+id).pipe((res: any)=>{
     return res
   })
  }
